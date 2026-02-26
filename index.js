bad4a7cbd870a628d0ac2bd1c4617f9aaf8860bc
Felipe Borges <felipecamposborgesinf@gmail.com>
2026-02-26 20:06:01 -0300

    Add smooth scrolling, mobile menu, and WhatsApp form
    Implement smooth scrolling for anchors, mobile menu toggle, and WhatsApp message submission.

---
+++ index.js
@@ -0,0 +1,42 @@
+// Scroll suave
+document.querySelectorAll('a[href^="#"]:not(.whatsapp)').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+// MENU MOBILE
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+// ENVIO PARA WHATSAPP
+document.getElementById("orcamentoForm").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const numero = "5551996048894";
+
+    const servico = document.getElementById("servico").value;
+    const nome = document.getElementById("nome").value;
+    const descricao = document.getElementById("descricao").value;
+
+    const mensagem = 
+`Olá, gostaria de solicitar um orçamento.
+
+*Nome/Empresa:* ${nome}
+*Tipo de Serviço:* ${servico}
+
+*Descrição:* ${descricao}
+
+Aguardo retorno.`;
+
+    const link = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+    window.open(link, "_blank");
+});
